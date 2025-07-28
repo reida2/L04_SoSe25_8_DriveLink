4792148a0bbfb688b28d832715501f31019a91f2
reida2 <ridaucb@gmail.com>
2025-07-28 12:38:41 +0200

    Docker is working and the projekt is up to date

---
+++ Frontend/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Frontend startup helper
+#
+# The Compose file passes FRONTEND_PORT (the host port you map
+# to container port 80).  Default to 80 if it isn’t set.
+#
+
+PORT="${FRONTEND_PORT:-80}"
+echo "⚡  Frontend is running on  http://localhost:${PORT}"
+
+# Hand off to Nginx (same as the original CMD)
+exec nginx -g 'daemon off;'
